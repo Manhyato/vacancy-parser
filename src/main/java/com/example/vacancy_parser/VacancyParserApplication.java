16027f6f3ca7c69d6fc5b0d69dc77892631e123f
Manhyato <145689933+Manhyato@users.noreply.github.com>
2025-11-05 15:10:28 +0300

    added a third task

--- src/main/java/com/example/vacancy_parser/VacancyParserApplication.java
+++ src/main/java/com/example/vacancy_parser/VacancyParserApplication.java
@@ -4,27 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import com.example.vacancy_parser.util.ThreadManager;
-import com.example.vacancy_parser.util.ThreadStateDemo;
 
 @SpringBootApplication
 public class VacancyParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VacancyParserApplication.class, args);
-	}
-	/*
-	@Bean
-	public CommandLineRunner  demoRunnerTask1(ThreadManager threadManager){
-		return args -> {
-			threadManager.startDemo();
-		};
-	}
-	*/
-	@Bean
-	public CommandLineRunner demoRunner(ThreadStateDemo demo) {
-    	return args -> {
-        	demo.runDemo();
-    	};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyParserApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(StreamComparisonService service) {
+        return args -> service.runComparison();
+    }
 }
